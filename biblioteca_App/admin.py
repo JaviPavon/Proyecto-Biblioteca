89947ef70f8116c1bcfaf58704385cc47819d20d
javipavon <xjpavbor068@ieshnosmachado.org>
2023-11-23 12:52:39 +0100

    Update Admin.py

--- biblioteca_App/admin.py
+++ biblioteca_App/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+# biblioteca_app/admin.py
+from django.contrib import admin
+from .models import Usuario, Autor, Editorial, Libro, Prestamo
+
+admin.site.register(Usuario)
+admin.site.register(Autor)
+admin.site.register(Editorial)
+admin.site.register(Libro)
+admin.site.register(Prestamo)
